0d3709fdbb745a19452a94af5b0520bc41bf2a04
firespace <FireSpace666@gmail.com>
2014-05-16 03:27:02 +0400

    while(1) -> while(! eof)

--- charbychar/charbychar.c
+++ charbychar/charbychar.c
@@ -3,12 +3,9 @@
 
 int main()
 {
-    while(1)
+    char buf[1];
+    while(wrap_read(STDIN_FILENO, buf, 1))
     {
-        char* buf = wrap_malloc(1);
-        if (wrap_read(STDIN_FILENO, buf, 1))
-            while (!wrap_write(STDOUT_FILENO, buf, 1)) {  }
-
+        write_all_data(STDOUT_FILENO, buf, 1);
     }
-
 }
