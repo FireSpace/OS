3cd4d75147d2af49c07da127a20ef0a0fa73544d
firespace <FireSpace666@gmail.com>
2014-05-16 22:24:32 +0400

    charbychar hard version

--- charbychar/charbychar.c
+++ charbychar/charbychar.c
@@ -1,11 +1,30 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "syscall_wrappers.h"
 
-int main()
+int main(int argc, char **argv)
 {
-    char buf[1];
-    while(wrap_read(STDIN_FILENO, buf, 1))
+    //if (argc < 3 || argc%2 == 0) { printf("bad arguments"); return 1; }
+
+    int steps = (argc-1) / 2;
+    int step = 0;
+    while (1)
     {
-        write_all_data(STDOUT_FILENO, buf, 1);
+        int data_len = (step < steps) ? atoi(argv[step*2]) : 1;
+        int sleep_time = (step < steps) ? atoi(argv[step*2+1]) : 0;
+        step += (step < steps) ? 1 : 0;
+
+        char buff[data_len];
+        int read_len = 0;
+        while ((data_len - read_len) > 0)
+        {
+            int ret = wrap_read(STDIN_FILENO, buff + read_len, data_len - read_len);
+            if (ret == 0) { close(STDOUT_FILENO); return 0; }
+            read_len += ret;
+        }
+
+        write_all_data(STDOUT_FILENO, buff, data_len);
+        sleep(sleep_time);
     }
 }
